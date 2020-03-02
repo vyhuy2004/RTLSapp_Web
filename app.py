d510655b0302828c2ac8abe944573984b06d3c4e
vyhuy2004 <vyhuy2004@gmail.com>
2020-03-02 00:33:14 -0800

    Add postgresql

--- app.py
+++ app.py
@@ -16,8 +16,28 @@ def close_db(error):
 
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+#    db = get_db()
 
+#    loc_cur = db.execute('''select * from location''')
+#    loc_result = loc_cur.fetchall()
+
+    return render_template('index.html')
+@app.route('/query')
+def query():
+    id = request.args.get('id')
+    x = request.args.get('x')
+    y = request.args.get('y')
+    z = request.args.get('z')
+    db = get_db()
+    db.execute('insert into location(id, x, y, z) values (%s, %s, %s, %s);', (id, x, y, z,))
+    return '<h1>{} {} {} {}</h1>'.format(id, x, y, z)
+
+@app.route('/viewresult')
+def viewresult():
+    db = get_db()
+    db.execute('select id, x, y, z from location')
+    results = db.fetchall()
+    return '<h1>ID: {}. X: {}. Y: {}. Z: {}</h1>'.format(results[1]['id'], results[1]['x'], results[1]['y'], results[1]['z'])
 
 if __name__ == '__main__':
     app.run()
