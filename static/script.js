f0ce017c9f20a2e0aa2d6e3baa29c412f0942e0a
vyhuy2004 <vyhuy2004@gmail.com>
2020-03-02 23:20:32 -0800

    Fix https

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var socket = io.connect('http://' + document.domain + ':' + location.port);
+  var socket = io.connect('https://' + document.domain + ':' + location.port);
 
   socket.on('connect', function() {
       socket.send('I am now connected!');
