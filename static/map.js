dbeeff7df2864a0965b79d181d52239ee9b8bdfe
vyhuy2004 <vyhuy2004@gmail.com>
2020-03-23 14:21:00 -0700

    Add remove tracking

--- static/map.js
+++ static/map.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
   var map = L.map('mapid', {
     crs: L.CRS.Simple,
   });
+  var current_loc = {};
   var bounds = [[-18,-75.5], [718-18,1285-75.5]];
   var image = L.imageOverlay('/static/Outline.jpg', bounds).addTo(map);
-  
+
   var socket = io.connect();
 
   socket.on('connect', function() {
@@ -22,8 +23,11 @@ $(document).ready(function(){
       var y = msg.y;
       var z = msg.z;
       var sol = L.latLng([ y,x ]);
-      L.circleMarker(sol,{radius: 5}).addTo(map);
-      map.fitBounds(bounds);
+      if(current_loc != undefined)
+      {
+        map.removeLayer(current_loc);
+      }
+      current_loc = L.circleMarker(sol,{radius: 5}).addTo(map);
   });
 
 });
