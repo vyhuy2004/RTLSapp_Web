04f01974f4d72c939636b4f32a323b4b80f1177a
vyhuy2004 <vyhuy2004@gmail.com>
2020-03-23 13:39:37 -0700

    Fix error map not initialized

--- static/map.js
+++ static/map.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
+  var map = L.map('mapid', {
+    crs: L.CRS.Simple,
+  });
+  var bounds = [[-18,-75.5], [718-18,1285-75.5]];
+  var image = L.imageOverlay('/static/Outline.jpg', bounds).addTo(map);
+  
   var socket = io.connect();
 
   socket.on('connect', function() {
       socket.send('Map now connected!');
-      var map = L.map('mapid', {
-        crs: L.CRS.Simple,
-      });
-      var bounds = [[-18,-75.5], [718-18,1285-75.5]];
-      var image = L.imageOverlay('/static/Outline.jpg', bounds).addTo(map);
       var sol = L.latLng([ 0,0 ]);
       L.circleMarker(sol,{radius: 5}).addTo(map);
       map.fitBounds(bounds);
